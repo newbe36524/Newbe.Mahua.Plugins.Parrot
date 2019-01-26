8c68557a21f05484b79baa75a52f3c9be04a8426
newbe36524 <472158246@qq.com>
2019-01-26 20:32:15 +0800

    Winform NET461

---
+++ src/CSharp/WinFormNET461/Newbe.Mahua.Plugins.Parrot/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Windows.Forms;
+
+namespace Newbe.Mahua.Plugins.Parrot
+{
+    public partial class MainForm : Form
+    {
+        private readonly NancyHost _nancyHost;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _nancyHost = new NancyHost(new Uri("http://localhost:65321"));
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _nancyHost.Dispose();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _nancyHost.Start();
+        }
+    }
+}
---
+++ src/CSharp/WinFormNET461/README.md
@@ -0,0 +1,13 @@
+# Webform Framework 3.5
+
+## generateClient.cmd
+
+用于生成 HTTP API 客户端代理类
+
+## Newbe.Mahua.Plugins.Parrot
+
+基于 Webform 的项目。需要安装 Framework 3.5 才能编译运行。
+
+可以使用 IDE 打开解决方案，编译启动该项目。
+
+可以使用 IISExpress 或者 IIS 启动，端口需要设置为 65321。
